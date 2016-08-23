dcfea1c705fa4f28b7ecb7fdf397d4d6398a7fd4
root <root@localhost.localdomain>
2016-08-23 21:14:36 +0800

    update

--- public/ui/CommentList.js
+++ public/ui/CommentList.js
@@ -6,14 +6,14 @@ let CommentList = React.createClass({
 	render(){
 		var commentNodes = this.props.data.map(function(comment){
 			return (
-				<Comment author={comment.author}>
+				<Comment key={comment.author} author={comment.author}>
 					{comment.text}
 				</Comment>
 			);
 		});
 
 		return (
-			<div class="commentList">
+			<div className="commentList">
 				{commentNodes}
 			</div>
 		);
