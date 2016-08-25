2604dc0666fde930950619a601e47d112671faa0
sqsa110 <254750295@qq.com>
2016-08-25 14:15:23 +0800

    updata

--- public/javascripts/backstagemain.js
+++ public/javascripts/backstagemain.js
@@ -1,34 +1,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import CommentBox from '../ui/CommentBox.js'
+import LoginBox from '../ui/login/LoginBox.js'
 
 let main = function(){
     render(
-        <CommentBox url="/api/comments" pollInterval={200000} />,
+        <LoginBox />,
         document.getElementById('content')
     );
 }
 
 main();
-
-/*
-var React = require('react');
-import { render } from 'react-dom';
-var HelloworldComponent = React.createClass({
-    displayName : 'HelloWorldComponent',
-    render : function(){
-        return (
-            <div>Hello world</div>
-        );
-    }
-});
-
-let main = function(){
-    render(
-        <HelloworldComponent />,
-        document.getElementById('example')
-    );
-}
-main();
-*/
--- public/javascripts/reactmain.js
+++ public/javascripts/reactmain.js
@@ -11,24 +11,3 @@ let main = function(){
 }
 
 main();
-
-/*
-var React = require('react');
-import { render } from 'react-dom';
-var HelloworldComponent = React.createClass({
-    displayName : 'HelloWorldComponent',
-    render : function(){
-        return (
-            <div>Hello world</div>
-        );
-    }
-});
-
-let main = function(){
-    render(
-        <HelloworldComponent />,
-        document.getElementById('example')
-    );
-}
-main();
-*/
