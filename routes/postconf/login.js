20dcf9c4640d2b061f43630ad72b73865814e201
sqsa110 <254750295@qq.com>
2016-09-05 00:06:24 +0800

    updata

--- routes/postconf/login.js
+++ routes/postconf/login.js
@@ -40,7 +40,7 @@ function Login(req,res){
 	function asciiToString(str,num){
 		var strArr = str.split('|');
 		var newstr = '';
-		for(var i=0,maxi=strArr.length-2;i<maxi;i++){
+		for(var i=0,maxi=strArr.length-1;i<maxi;i++){
 			newstr += String.fromCharCode(strArr[i] - num);
 		}
 		console.log(maxi);
