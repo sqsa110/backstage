602de607f40df1c1df77de850e4c3a8efff4ed50
sqsa110 <254750295@qq.com>
2016-09-04 16:07:57 +0800

    updata

--- routes/postconf/login.js
+++ routes/postconf/login.js
@@ -4,10 +4,10 @@ var myconf = require('../conf');
 function Login(req,res){
 	console.log(req.body);
 	var tables = 'users';
-	var sql= "select * from ? ";
+	var sql= "select * from users ";
 	var connection = mysql.createConnection(myconf.mysqlconf);
 	connection.connect();
-	connection.query(sql,[tables],function(err,row,fields){
+	connection.query(sql,function(err,row,fields){
 		if(err){
 			throw err;
 		}
