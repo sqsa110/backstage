00ccd27a04fc875a78d225c402cc7635874626c5
sqsa110 <254750295@qq.com>
2016-09-16 22:17:36 +0800

    updata

--- public/javascripts/ant.js
+++ public/javascripts/ant.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Box extends React.Component{
-    render(){
-      return (
-        <div className="modal-header">
-          <div className="container-fluid">
-            <div className="row title">
-              <div className="col-xs-6"><a href="javascript:;">登陆</a></div>
-              <div className="col-xs-6"><a href="javascript:;">快速注册</a></div>
-            </div>
-            <div className="row dash">
-              <div className="col-xs-2"></div>
-              <div className="col-xs-2 line"></div>
-              <div className="col-xs-2"></div>
-              <div className="col-xs-2"></div>
-              <div className="col-xs-2 line"></div>
-              <div className="col-xs-2"></div>
-            </div>
-          </div>
-        </div>
-      )
-    }
-};
+class Main extends React.Component{
+  render(){
+    return (
+      <div>123
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(
-	<Box />,
-	document.getElementById('login')
-);
+ReactDOM.render(<Main />,document.getElementById('login'))
