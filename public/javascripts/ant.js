3156b2bf3cc1dec6085da6d7a4a48904443d1e55
sqsa110 <254750295@qq.com>
2016-09-17 20:13:19 +0800

    updata

--- public/javascripts/ant.js
+++ public/javascripts/ant.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import LoginModal from 'modal.js';
+import LoginModal from './modal.js';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(<LoginModal />, document.getElementById('login'));
