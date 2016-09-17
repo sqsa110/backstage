4eecd5dabf0c9e626cfc362045fe475422949cda
sqsa110 <254750295@qq.com>
2016-09-17 20:04:26 +0800

    updata

---
+++ public/javascripts/modal.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Modal, Button } from 'antd';
+import Register from './register.js';
+
+const LoginModal = React.createClass({
+  getInitialState() {
+    return {
+      loading: false,
+      visible: false,
+    };
+  },
+  showModal() {
+    this.setState({
+      visible: true,
+    });
+  },
+  handleOk() {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  },
+  handleCancel() {
+    this.setState({ visible: false });
+  },
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          显示对话框
+        </Button>
+        <Modal ref="modal"
+          visible={this.state.visible}
+          title="对话框标题" onOk={this.handleOk} onCancel={this.handleCancel}
+          footer={[
+            <Button key="back" type="ghost" size="large" onClick={this.handleCancel}>返 回</Button>,
+            <Button key="submit" type="primary" size="large" loading={this.state.loading} onClick={this.handleOk}>
+              提 交
+            </Button>,
+          ]}
+        >
+          <Register />
+        </Modal>
+      </div>
+    );
+  },
+});
+
+export default = LoginModal;
+
