81c55e806f417799f080e021282a1e602c5882ed
sqsa110 <254750295@qq.com>
2016-09-20 20:26:10 +0800

    updata

--- public/javascripts/antbox.js
+++ public/javascripts/antbox.js
@@ -15,13 +15,13 @@ let AntdBox = React.createClass({
             <div className="ant-layout-logo"></div>
             <Menu mode="inline" theme="dark"
               defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']}>
-              <SubMenu key="sub1" title={<span><Icon type="user" />导航一</span>}>
-                <Menu.Item key="1">选项1</Menu.Item>
-                <Menu.Item key="2">选项2</Menu.Item>
+              <SubMenu key="sub1" title={<span><Icon type="user" />导航一</span>} onTitleClick={function(obj){console.log(obj)}}>
+                <Menu.Item key="1" onClick={function(){console.log(1)}}>选项1</Menu.Item>
+                <Menu.Item key="2" onClick={function(){console.log(2)}}>选项2</Menu.Item>
                 <Menu.Item key="3">选项3</Menu.Item>
                 <Menu.Item key="4">选项4</Menu.Item>
               </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />导航二</span>}>
+              <SubMenu key="sub2" title={<span><Icon type="laptop" />导航二</span>} onTitleClick={function(obj){console.log(obj)}}>
                 <Menu.Item key="5">选项5</Menu.Item>
                 <Menu.Item key="6">选项6</Menu.Item>
                 <Menu.Item key="7">选项7</Menu.Item>
