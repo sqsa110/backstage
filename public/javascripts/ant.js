ac5d4ee976ee7602a7ef971707175c9ec554f836
sqsa110 <254750295@qq.com>
2016-09-23 12:20:40 +0800

    updata

--- public/javascripts/ant.js
+++ public/javascripts/ant.js
@@ -13,6 +13,7 @@ var routersArr = [];
 for(var i=0,maxi=navData.length;i<maxi;i++){
   for(var j=0,maxj=navData[i].data.length;i<maxj;j++){
     console.log(navData[i].data[j]);
+    console.log(i,j)
     routersArr.push(<Route path={navData[i].data[j].link} component={navData[i].data[j].component} />)
   }
 }
